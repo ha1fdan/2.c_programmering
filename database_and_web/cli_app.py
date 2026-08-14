7be415d406173cbe29acea1a4ada696953651219
ha1fdan <ha1fdan@ha1fdan.xyz>
2026-08-14 15:53:00 +0200

    added delete category option

--- database_and_web/cli_app.py
+++ database_and_web/cli_app.py
@@ -11,6 +11,7 @@ while True:
     print("1. Search customers")
     print("2. Create category")
     print("3. Show categories")
+    print("4. Delete category")
 
     opt = int(input("? "))
 
@@ -24,10 +25,19 @@ while True:
                 print(f"[{row['CustomerID']}]: {row['CustomerName']} | {row['Address']}")
     elif opt == 2:
         catname = input("Category name: ")
-        cur.execute("INSERT INTO Categories (CategoryName) VALUES (?)", (catname,))
+        catdesc = input("Category description: ")
+        cur.execute("INSERT INTO Categories (CategoryName, Description) VALUES (?, ?)", (catname, catdesc))
         con.commit() # Commit when changing data
         print(f"Category '{catname}' created.")
     elif opt == 3:
         res = cur.execute("SELECT * FROM Categories;")
         for row in res.fetchall():
             print(row['CategoryId'], row['CategoryName'], row['Description'])
+    elif opt == 4:
+        #list categories and then ask for id to delete
+        res = cur.execute("SELECT * FROM Categories;")
+        for row in res.fetchall():
+            print(f"[{row['CategoryId']}]: {row['CategoryName']} | {row['Description']}")
+        catid = int(input("Category ID to delete: "))
+        cur.execute("DELETE FROM Categories WHERE CategoryId = ?", (catid,))
+        con.commit()
